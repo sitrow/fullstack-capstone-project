cc3fa73080fcd5e06db560a37e4c2c7af85f52e0
sitrow <strbaz@outlook.com>
2024-05-13 15:50:41 -0400

    registration added

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import './RegisterPage.css';
 
 function RegisterPage() {
@@ -8,10 +10,46 @@ function RegisterPage() {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState('');
+    
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
 
     // insert code here to create handleRegister function and include console.log
+
     const handleRegister = async () => {
-        console.log("Register invoked")
+        try{
+            const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password
+                }),
+            });
+
+            const json = await response.json();
+            console.log('json data', json);
+            console.log('er', json.error);
+
+            if(json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', firstName);
+                sessionStorage.setItem('email', json.email);
+                setIsLoggedIn(true);
+                navigate('/app');
+            }
+            if (json.error) {
+                setShowerr(json.error);
+            }
+    
+        } catch(error) {
+            console.log("Error fetching details: " + error.message);
+        }
     }
 
     return (
@@ -32,6 +70,7 @@ function RegisterPage() {
                         <div className="mb-4">
                             <label htmlFor="email" className="form-label">Email</label>
                             <input id="email" type="text" className="form-control" placeholder="Enter your Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                            <div className="text-danger">{showerr}</div>
                         </div>
                         <div className="mb-4">
                             <label htmlFor="password" className="form-label">Password</label>
